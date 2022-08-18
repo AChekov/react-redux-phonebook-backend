354ccbf49ce0052350c592b641c553d8bbfa8a38
Chekov Alexei <4ekoff@gmail.com>
2022-08-18 10:46:46 +0300

    create API

---
+++ src/services/API-Contacts.jsx
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://62fde6f641165d66bfb3e2d1.mockapi.io/';
+
+export const fetchContacts = async () => {
+  const { data } = await axios.get('/contacts');
+  return data;
+};
